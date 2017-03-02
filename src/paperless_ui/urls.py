12b02b55a2dd7d29edfb91b6439e7ac018425dfc
Philippe O. Wagner <wagner@arteria.ch>
2017-03-02 13:01:33 +0100

    .

--- src/paperless_ui/urls.py
+++ src/paperless_ui/urls.py
@@ -6,4 +6,5 @@ from . import views
 
 urlpatterns = [
     url(r'^all/$', views.index, name='paperless_ui_index'),
+    url(r'^view/(?P<checksum>\w+)/$', views.doc_viewer, name='paperless_ui_view'),
 ]
--- src/paperless_ui/views.py
+++ src/paperless_ui/views.py
@@ -9,3 +9,16 @@ def index(request):
     docs = Document.objects.all()
     context = {'docs': docs, }
     return render(request, 'paperless_ui/docs/index.html', context)
+
+
+@login_required
+def doc_viewer(request, checksum):
+    doc = Document.objects.get(checksum=checksum)
+
+    from markdownx.settings import MARKDOWNX_MARKDOWNIFY_FUNCTION
+    from django.utils.module_loading import import_string
+    markdownify = import_string(MARKDOWNX_MARKDOWNIFY_FUNCTION)
+    md_content = markdownify(doc.content)
+
+    context = {'doc': doc, 'md_content': md_content,}
+    return render(request, 'paperless_ui/docs/doc_viewer.html', context)
