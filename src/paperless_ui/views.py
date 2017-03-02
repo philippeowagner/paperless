28eca462632e91f7bcbb9990e40d069db85ff9f3
Philippe O. Wagner <wagner@arteria.ch>
2017-03-02 12:22:10 +0100

    .

--- src/paperless_ui/views.py
+++ src/paperless_ui/views.py
@@ -2,7 +2,10 @@ from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from documents.models import *
+
 @login_required
 def index(request):
-    context = {}
+    docs = Docuements.objects.all()
+    context = {'docs': docs, }
     return render(request, 'paperless_ui/docs/index.html', context)
